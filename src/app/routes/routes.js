f9cf6efa0f2871834f8c64c87ee7646d050fed17
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2019-12-31 15:52:48 -0600

    Included routes file

---
+++ src/app/routes/routes.js
@@ -0,0 +1,28 @@
+module.exports = app => {
+  app.get("/", (req, res) => {
+    res.send(`
+              <html>
+                  <head>
+                      <meta charset='utf-8'>
+                  </head>
+                  <body>
+                      <h1>Casa do Código</h1>
+                      <h2>Home Page</h2>
+                  </body>
+              </html>
+          `);
+  });
+
+  app.get("/livros", (req, res) => {
+    res.send(`
+                <html>
+                    <head>
+                        <meta charset='utf-8'>
+                    </head>
+                    <body>
+                        <h1>Livros</h1>
+                    </body>
+                </html>
+            `);
+  });
+};
