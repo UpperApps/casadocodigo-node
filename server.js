fdf739a3b9f10c3d965f0dc69d907fe0933f8531
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2019-12-31 15:51:58 -0600

    Finished implementation

--- server.js
+++ server.js
@@ -4,30 +4,3 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is working on port 3000`);
 });
-
-app.get("/", (req, res) => {
-  res.send(`
-        <html>
-            <head>
-                <meta charset='utf-8'>
-            </head>
-            <body>
-                <h1>Casa do Código</h1>
-                <h2>Home Page</h2>
-            </body>
-        </html>
-    `);
-});
-
-app.get("/livros", (req, res) => {
-    res.send(`
-          <html>
-              <head>
-                  <meta charset='utf-8'>
-              </head>
-              <body>
-                  <h1>Livros</h1>
-              </body>
-          </html>
-      `);
-  });
